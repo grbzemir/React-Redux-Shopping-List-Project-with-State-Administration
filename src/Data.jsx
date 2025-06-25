77f1323dfee08b155d57ab9bc7ef566c19df3167
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-25 21:36:32 +0300

    FakeData

---
+++ src/Data.jsx
@@ -0,0 +1,40 @@
+const Data = [
+    {
+        id: 1,
+        name: 'Selpak Paper Towel',
+        price: 154.99,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf9koMbhIrGMWTsuIEtRNqxv33QgsP51yZkQ&s'
+    },
+    {
+        id: 2,
+        name: 'Coca Cola',
+        price: 5.99,
+        image: 'https://www.nazar.com.tr/wp-content/uploads/2020/12/coca-cola-1-litre.jpg'
+    },
+    {
+        id: 3,
+        name: 'Fanta',
+        price: 5.99,
+        image: 'https://www.pasaportpizzaakcay.com/wp-content/uploads/2023/09/fanta-1lt.jpeg'
+    },
+    {
+        id: 4,
+        name: 'Pedigree Biftekli Kuru Mama 2.6kg',
+        price: 79.99,
+        image: 'https://img.gratis.com/mnpadding/800/800/ffffff/h59/h79/8816313958430/10319179_03.jpg'
+    },
+    {
+        id: 5,
+        name: 'Whiskas Kedi Maması 1.75kg',
+        price: 49.99,
+        image: 'https://ardenmarket.com.tr/media/catalog/product/cache/0b154ba1d9ffbc98998b9163ce10b49b/k/e/kedi-mamasi-kuru-ton.jpg'
+    },
+    {
+        id: 6,
+        name: 'Papia Toilet Paper',
+        price: 49.99,
+        image: 'https://semtgida.com/wp-content/uploads/2018/09/PAPIA-TOILET-PAPER-32-PERFUMED.jpg'
+    }
+];
+
+export default Data;
