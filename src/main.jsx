07915010f810d1682cb25c1b4d4835fdec594dee
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-25 21:36:53 +0300

    Provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore } from 'redux'
+import { Reducers } from './Redux/reducers/Reducers.jsx'
+
+const store = createStore(Reducers)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
