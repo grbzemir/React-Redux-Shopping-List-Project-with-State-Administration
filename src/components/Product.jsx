efc2a069d49659a133c018573d975e3faed78d1a
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-25 22:58:30 +0300

    Product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { addBasket } from '../Redux/actions/Actions';
 
-const Product = () => {
+const Product = (props) => {
+    // console.log(props.ProductsList);
     return (
         <div>
-            <h1>
+            <h2>
                 <span>Alınacaklar Listem</span>
                 <Link to='/basket'>Faturam &#10095;</Link>{''}
-            </h1>
+            </h2>
+            {
+                props.ProductsList.map((product, index) => {
+                    return (
+                        <div className='product' key={index}>
+                            <img src={product.image} alt={product.name} />
+                            <div>
+                                <h4>{product.name}</h4>
+                                <p>{product.price} ₺</p>
+                                <button onClick={() => props.addBasket(product)}>Sepete Ekle</button>
+                            </div>
+                        </div>
+                    );
+                })
+            }
         </div>
     )
 }
 
-export default Product
+const mapStateToProps = (state) => {
+    return {
+        ProductsList: state.ProductsList,
+        Basket: state.Basket
+    }
+}
+
+export default connect(mapStateToProps, { addBasket })(Product) 
