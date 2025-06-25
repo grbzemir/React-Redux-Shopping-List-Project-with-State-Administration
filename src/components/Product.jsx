396911bb1403694ee8ce1bf0276a5a809fc0a598
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-25 21:36:45 +0300

    Product

---
+++ src/components/Product.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Product = () => {
+    return (
+        <div>
+            <h1>
+                <span>Alınacaklar Listem</span>
+                <Link to='/basket'>Faturam &#10095;</Link>{''}
+            </h1>
+        </div>
+    )
+}
+
+export default Product
