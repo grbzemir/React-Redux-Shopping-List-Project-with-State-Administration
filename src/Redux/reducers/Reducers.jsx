1f6ba85e540796031954c1077da1d00246168880
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-06-25 22:58:42 +0300

    Reducers

--- src/Redux/reducers/Reducers.jsx
+++ src/Redux/reducers/Reducers.jsx
@@ -1,20 +1,28 @@
-import { act } from "react";
-import Product from "../../components/Product";
 import Data from "../../Data";
 
 const initialState = {
     ProductsList: Data,
     Basket: [],
-
-}
+};
 
 export const Reducers = (state = initialState, action) => {
     switch (action.type) {
         case "ADD_BASKET":
             return { ...state, Basket: [...state.Basket, action.payload] };
 
+        case "REMOVE_BASKET":
+            const index = state.Basket.findIndex(item => item.id === action.payload);
+            if (index >= 0) {
+                const newBasket = [...state.Basket];
+                newBasket.splice(index, 1);
+                return { ...state, Basket: newBasket };
+            }
+            return state;
+
+        case "CLEAR_BASKET":
+            return { ...state, Basket: [] };
+
         default:
             return state;
     }
-
-}
+};
